c0f626b6b43d6e9abbf9e571e4e1a32f4e18cb36
Dian Nikolov <dian.nikolov91@gmail.com>
2016-06-11 11:18:57 +0300

    Added second column for image and proper image scaling

--- src/EventHandlers/EventHandlers.hs
+++ src/EventHandlers/EventHandlers.hs
@@ -5,6 +5,7 @@ import Graphics.UI.WX
 import Graphics.UI.WXCore
 
 -- Specify image files for the file open dialog.
+imageFiles :: [(String, [String])]
 imageFiles
    = [("Image files",["*.bmp","*.jpg","*.gif","*.png"])
      ,("Portable Network Graphics (*.png)",["*.png"])
@@ -12,6 +13,18 @@ imageFiles
      ,("JPG files (*.jpg)",["*.jpg"])
      ,("GIF files (*.gif)",["*.gif"])
      ]
+
+-- TODO: Move to another source file
+getImageScale :: Size -> Size -> Size 
+getImageScale imageSize windowSize
+  = let 
+        imageWidth   = sizeW imageSize
+        imageHeight  = sizeH imageSize
+        windowWidth  = sizeW windowSize
+        windowHeight = sizeH windowSize
+        newWidth     = (imageWidth  * min windowWidth windowHeight) `div` max imageWidth imageHeight
+        newHeight    = (imageHeight * min windowWidth windowHeight) `div` max imageWidth imageHeight
+    in Size newWidth newHeight
      
 onOpen :: Frame a -> ScrolledWindow b -> Var (Maybe (Bitmap ())) -> MenuItem c -> StatusField -> IO ()
 onOpen f sw vbitmap mclose status
@@ -39,8 +52,9 @@ openImage :: ScrolledWindow a -> Var (Maybe (Bitmap ())) -> MenuItem b -> Status
 openImage sw vbitmap mclose status fname
   = do -- load the new bitmap
        let img = image fname
-       Size w h <- get img size
-       imgScaled <- imageConvertToBitmap =<< imageScale img (sz (w `div` h * 500) 500)
+       Size imgW imgH <- get img size
+       Size clientW clientH  <- get sw clientSize
+       imgScaled <- imageConvertToBitmap =<< imageScale img (getImageScale (sz imgW imgH) (sz clientW clientH))
        closeImage vbitmap
        set vbitmap [value := Just imgScaled]
        set mclose [enabled := True]
@@ -52,7 +66,14 @@ openImage sw vbitmap mclose status fname
    `onException` repaint sw
 
 onPaint :: Var (Maybe (Bitmap ())) -> DC () -> Rect -> IO ()
-onPaint vbitmap dc viewArea
+onPaint vbitmap dc _
+  = do mbBitmap <- get vbitmap value
+       case mbBitmap of
+         Nothing -> return () 
+         Just bm -> drawBitmap dc bm pointZero False []
+
+onPaintSecond :: Var (Maybe (Bitmap ())) -> DC () -> Rect -> IO ()
+onPaintSecond vbitmap dc _
   = do mbBitmap <- get vbitmap value
        case mbBitmap of
          Nothing -> return () 
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import Control.Exception (onException)
 import Graphics.UI.WX
-import Graphics.UI.WXCore
 import EventHandlers
 
 main :: IO ()
@@ -13,7 +11,7 @@ main
 imageViewer :: IO ()
 imageViewer
   = do -- the main frame, we use 'fullRepaintOnResize' to prevent flicker on resize
-       f      <- frame [text := "ImageViewer", picture := "images/image.jpg", fullRepaintOnResize := False]
+       f      <- frameFixed [text := "TexMage", picture := "images/image.jpg", fullRepaintOnResize := False]
 
        -- use a mutable variable to hold the image
        vbitmap <- variable [value := Nothing]
@@ -22,33 +20,45 @@ imageViewer
        sw     <- scrolledWindow f [scrollRate := sz 10 10, 
                                    on paint := onPaint vbitmap,
                                    bgcolor := white, fullRepaintOnResize := False]
+       swSec  <- scrolledWindow f [scrollRate := sz 10 10, 
+                                   on paint := onPaintSecond vbitmap,
+                                   bgcolor := white, fullRepaintOnResize := False]
 
        -- create file menu
        file   <- menuPane      [text := "&File"]
        mclose <- menuItem file [text := "&Close\tCtrl+C", help := "Close the image", enabled := False]
        open   <- menuItem file [text := "&Open\tCtrl+O",  help := "Open an image"]
        menuLine file
-       quit   <- menuQuit file [help := "Quit the demo"]
+       quit   <- menuQuit file [help := "Quit"]
 
        -- create Help menu
        hlp    <- menuHelp      []
-       about  <- menuAbout hlp [help := "About ImageViewer"]
+       about  <- menuAbout hlp [help := "About TexMage"]
 
        -- create Toolbar
        tbar   <- toolBar f []
-       toolMenu tbar open  "Open"  "images/image.jpg" []
+       _      <- toolMenu tbar open  "Open"  "images/image.jpg" []
 
        -- create statusbar field
-       status <- statusField   [text := "Welcome to the wxHaskell ImageViewer"]
+       status <- statusField   [text := "Welcome to TexMage"]
 
        -- set the statusbar, menubar, layout, and add menu item event handlers
        -- note: set the layout before the menubar!
-       set f [layout           := column 1 [hfill $ hrule 1,  -- add divider between toolbar and scrolledWindow
-                                            fill (widget sw)],
+       set f [layout           := grid 1 1 [  -- TODO: Refactor into readable functions
+                                             [ hfill $ hrule 1 ],
+                                             [ grid 1 1 
+                                               [
+                                                [ column 1 [fill $ widget sw],
+                                                  vfill $ vrule 1,
+                                                  column 1 [fill $ widget swSec] 
+                                                ]
+                                               ]
+                                             ] 
+                                           ],
               statusBar        := [status],
               menuBar          := [file, hlp],
-              outerSize        := sz 800 800,    -- niceness
-              on (menu about)  := infoDialog f "About ImageViewer" "This is a wxHaskell demo",
+              outerSize        := sz 640 480,    -- niceness
+              on (menu about)  := infoDialog f "About TexMage" "TODO",
               on (menu quit)   := close f,
               on (menu open)   := onOpen f sw vbitmap mclose status,
               on (menu mclose) := onClose sw vbitmap mclose status,
